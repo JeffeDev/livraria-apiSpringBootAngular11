709340a8423c60ce7a831d7ffe146148139f2ace
JeffeDev <jeffe.info@gmail.com>
2022-08-05 01:17:36 -0300

    Finalizando as classes do dominio

--- src/main/java/br/com/jeffe/livraria/domain/Categoria.java
+++ src/main/java/br/com/jeffe/livraria/domain/Categoria.java
@@ -1,14 +1,26 @@
 package br.com.jeffe.livraria.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Categoria {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Categoria implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String descricao;
 	
+	@OneToMany(mappedBy = "categoria")
 	private List<Livro> livros = new ArrayList<>();
 
 	public Categoria(Long id, String nome, String descricao, List<Livro> livros) {
--- src/main/java/br/com/jeffe/livraria/domain/Livro.java
+++ src/main/java/br/com/jeffe/livraria/domain/Livro.java
@@ -1,13 +1,26 @@
 package br.com.jeffe.livraria.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Livro {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Livro implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String titulo;
 	private String nomeAutor;
 	private String texto;
 	
+	@ManyToOne @JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 
 	public Livro(Long id, String titulo, String nomeAutor, String texto, Categoria categoria) {
